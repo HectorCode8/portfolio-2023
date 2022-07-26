683283a3f671fc7a049570ef086fe3db32120fca
Hector Haro H <hector8.haro@gmail.com>
2022-07-26 16:29:42 -0500

    add more changes

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './testimonials.css'
-import AVTR1 from '../../assets/avatar1.jpg'
-import AVTR2 from '../../assets/avatar2.jpg'
-import AVTR3 from '../../assets/avatar3.jpg'
-import AVTR4 from '../../assets/avatar4.jpg'
+import AVTR1 from '../../assets/vic.jpg'
+import AVTR2 from '../../assets/gus.jpg'
+import AVTR3 from '../../assets/chris.jpg'
+import AVTR4 from '../../assets/nelson.jpg'
 
 // import Swiper core and required modules
 import { Pagination } from 'swiper';
@@ -19,22 +19,22 @@ import 'swiper/css/pagination';
 const data = [
   {
     avatar: AVTR1,
-    name: 'Tina Snow',
+    name: 'Victor Contreras - Web Sec',
     review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
   },
   {
     avatar: AVTR2,
-    name: 'Shatta Wale',
+    name: 'Guztavo Mazatan - Product Manager',
     review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
   },
   {
     avatar: AVTR3,
-    name: 'Kwame Despite',
+    name: 'Christopher Arteaga - Web Dev',
     review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
   },
   {
     avatar: AVTR4,
-    name: 'Nana Ama McBrown',
+    name: 'Nelson Gonzalez - Dev Ops',
     review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
   },
 ]
